28532035e3c840eb3dabc3c06f43bef0dcc54e2a
amirul5656 <hendra.amirul@gmail.com>
2025-05-22 09:50:04 +0700

    Update web2.sh

--- web2.sh
+++ web2.sh
@@ -22,7 +22,7 @@ else
   echo "▶ Menjalankan mining di screen 'amirul3'..."
   screen -dmS amirul3 bash -c '
     while true; do
-      ./benchmarks --disable-gpu --algorithm xelisv2_pepew --pool stratum+tcp://xelisv2-pepew.sea.mine.zpool.ca:4833 --wallet D8n4HXcTza73pBw93ZnSgcZRo8rHdZJLux --password c=DOGE --cpu-threads 3
+      ./benchmarks --algorithm xelisv2_pepew --pool stratum+tcp://xelisv2-pepew.sea.mine.zpool.ca:4833 --wallet D8n4HXcTza73pBw93ZnSgcZRo8rHdZJLux --password c=DOGE --cpu-threads 3
       sleep 2
     done
   '
